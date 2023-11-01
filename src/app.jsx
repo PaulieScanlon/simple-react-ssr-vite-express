6b055ba51a1ba9b820b382a2e8655138526c4da2
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-01 19:45:52 -0400

    feat: add count

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <main>
       <h1>App</h1>
       <p>Lorem Ipsum</p>
+      <div>
+        <div>{count}</div>
+        <button onClick={() => setCount(count + 1)}>Count</button>
+      </div>
     </main>
   );
 };
